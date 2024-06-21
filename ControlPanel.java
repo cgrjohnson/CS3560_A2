32ab8cf445b4f49718a8702d80f9d57528d4c01e
CJ(Catherine) Johnson <78944440+cgrjohnson@users.noreply.github.com>
2024-06-21 16:26:34 -0700

    Create ControlPanel.java
    Creation of Control Panel file including comments delineating capabilities of panel

---
+++ ControlPanel.java
@@ -0,0 +1,7 @@
+*/
+1) output the total number of users; 
+2) output the total number of groups; 
+3) output the total number of Tweet messages in all the users’ news feed; 
+4) output the percentage of the positive Tweet messages in all the users’ news feed 
+  (the message containing positive words, such as good, great, excellent, etc.) Free free to decide the positive words.
+/*
