b1546ce95d274e0fc4b28bf3326a4cbcd1e2659a
CJ(Catherine) Johnson <78944440+cgrjohnson@users.noreply.github.com>
2024-06-21 16:59:15 -0700

    Update TheVerse.java
    Added structure/placeholders to paste in classes to main file later

--- TheVerse.java
+++ TheVerse.java
@@ -1,2 +1,16 @@
 import javax.swing.*;
 
+//User Class
+//class User {}
+//above is a placeholder for once I'm done working on the class
+
+
+//User Group Class
+//class Group {}
+//above is a placeholder for once I'm done working on the class
+
+
+//Main Class for Mini Twitter application
+//class TheVerse {
+
+//}
