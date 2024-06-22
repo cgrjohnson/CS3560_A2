38a23dfcaef6685f548f4eda1d598f91170b91de
CJ(Catherine) Johnson <78944440+cgrjohnson@users.noreply.github.com>
2024-06-21 18:10:23 -0700

    Update TheVerse.java
    delcaration/init of main class variables/et c., basic GUI set up

--- TheVerse.java
+++ TheVerse.java
@@ -1,3 +1,4 @@
+
 import javax.swing.*;
 import java.util.List;
 import java.util.ArrayList;
@@ -12,7 +13,38 @@ import java.util.ArrayList;
 //above is a placeholder for once I'm done working on the class
 
 
-//Main Class for Mini Twitter application
-//class TheVerse {
+//Main Class for application
+/**/class TheVerse {
+    /*private Groop rootGroup;                                        //for the Root Group containing everything
+    private DefaultMutableTreeNode rootNode;                        //Node for Root Group
+    private DefaultTreeModel treeModel                              //Tree model for The Verse
+*/
+    public TheVerse() {                                             //public class TheVerse
+          //this.rootGroup = new Group("Root");                      
+          //this.rootNode = new DefaultMutableTreeNode(rootGroup);   
+          //      this.treeModel = new DefaultTreeModel(rootNode);
+        JFrame frame = new JFrame();
+      
+        JPanel panel = new JPanel();
+        panel.setborder(BorderFactory.creatEmptyBorder(30,30,10,30));
+        panel.setLayout(new GridLayout(0,  1));
 
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Welcome to The Verse");
+        frame.pack();
+        frame.setVisible(true);
+    }
+*/
+  //add in functions
+  //add in UI components(buttons, text input)
+
+
+/*public static void main(String[] args) {                              //displays control panel when you run prog
+  TheVerse theVerse = new TheVerse();
+  theVerse.displayControlPanel();
+*/
 //}
+//}
+
+  
