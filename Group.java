0344c3eff32a5a87b794ad6707c1307241d3c323
CJ(Catherine) Johnson <78944440+cgrjohnson@users.noreply.github.com>
2024-06-21 17:25:50 -0700

    Update Group.java
    declare and initialize for public class var and list for Group, placeholders for Group functions

--- Group.java
+++ Group.java
@@ -2,3 +2,17 @@
 //can contain any number of Users
 //users can only subscribe to one group
 //"root" group contains everything
+
+class Group {
+    private String id;                    //declare Group id var
+    private List<User> users;             //declare users list
+
+    public Group(String id) {             //public Group class
+        this.id = id;                     //initialize var and list for public Group
+        this.users = new ArrayList<>();
+    }
+
+    //getId
+    //getUsers
+    //add to user list
+}
